b9cc08cb0d1489f516182e9c16651eaa16de16e9
= <=>
2019-10-27 15:42:15 +0700

    add-all brand product, fix login button

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -278,6 +278,17 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 						<li><a href="{{URL::to('/all-category-product')}}">Hiển thị danh mục sản phẩm</a></li>
                     </ul>
                 </li>
+
+                <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class="fa fa-book"></i>
+                        <span>Thương hiệu sản phẩm</span>
+                    </a>
+                    <ul class="sub">
+						<li><a href="{{URL::to('/add-brand-product')}}">Thêm thương hiệu sản phẩm</a></li>
+						<li><a href="{{URL::to('/all-brand-product')}}">Hiển thị thương hiệu sản phẩm</a></li>
+                    </ul>
+                </li>
                 
             </ul>            
         </div>
@@ -299,9 +310,11 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			</div>
 		  </div>
   <!-- / footer -->
+
+    </section>
+
 </section>
-<!--main content end-->
-</section>
+
 <script src="{{asset('public/backend/js/bootstrap.js')}}"></script>
 <script src="{{asset('public/backend/js/jquery.dcjqaccordion.2.7.js')}}"></script>
 <script src="{{asset('public/backend/js/scripts.js')}}"></script>
